724f6249cd74d6b5626782991cb4f2f4b47f4829
Yusif Huseynli <yusifhuseynli@Yusifs-MacBook-Air.local>
2021-12-06 14:49:39 +0400

    some bugs

--- static/js/add-product.js
+++ static/js/add-product.js
@@ -1,5 +1,5 @@
-const markaUrl = 'http://127.0.0.1:8000/contact-api/main/'
-const modelUrl = 'http://127.0.0.1:8000/contact-api/main-model/'
+const markaUrl = 'http://127.0.0.1:8000/main-api/main/'
+const modelUrl = 'http://127.0.0.1:8000/en/main-api/main-model/'
 const addProductUrl = 'http://127.0.0.1:8000/product-api/add-product/'
 const categoryUrl = 'http://127.0.0.1:8000/product-api/category/'
 
