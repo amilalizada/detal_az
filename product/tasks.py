92f19209c786c6384680f2c22e19b82e98fc28fd
vusallyv <vusal581176@gmail.com>
2021-12-08 14:49:19 +0400

    Celery fixed

--- product/tasks.py
+++ product/tasks.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 @shared_task
 def check_active_products():
     startdate = timezone.now()
-    enddate = startdate - timedelta(months=1)
+    enddate = startdate - timedelta(days=30)
     products = Product.objects.filter(user_id__is_market=False, updated_at__lte=enddate)
     for product in products:
         product.is_active = False
