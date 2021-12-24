c899d0f7dab846b6b4477a473ad2c57d20d49a7a
yusif763 <yusifhuseynli1105@gmail.com>
2021-12-24 15:02:54 +0400

    staticfiles

--- detalaz/settings.py
+++ detalaz/settings.py
@@ -176,12 +176,6 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
-
 if PROD:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 else:
