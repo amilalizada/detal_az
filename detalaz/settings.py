836afb56f1507f6aa7292fe0df459ca6dca01700
yusif763 <yusifhuseynli1105@gmail.com>
2022-01-04 18:03:49 +0400

    some changes for csrf token

--- detalaz/settings.py
+++ detalaz/settings.py
@@ -179,9 +179,9 @@ STATIC_URL = '/static/'
 if PROD:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
+        os.path.join(BASE_DIR/ 'static')
     ]
 
 MEDIA_URL = '/media/'
